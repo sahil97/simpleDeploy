cd40cfe131cf865702f3229a9b2508cbecd9ab06
sahil97 <sahil.sharma071997@gmail.com>
2019-08-07 20:12:49 +0530

    added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
-import FileBrowser from "./containers/Main/Main";
+import { Switch, Route } from "react-router";
+import Dashboard from "./components/Dashboard/Dashboard";
+import ProjectsPage from "./components/ProjectPage/ProjectPage";
 
 function App() {
   return (
     <div className="App">
-      <FileBrowser />
+      <Switch>
+        <Route path="/projects" component={ProjectsPage} />
+        <Route path="/" exact component={Dashboard} />
+      </Switch>
     </div>
   );
 }
--- frontend/src/components/File/File.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const file = props => {
-  return <div onClick={props.onClick}>{props.filename}</div>;
-};
-
-export default file;
